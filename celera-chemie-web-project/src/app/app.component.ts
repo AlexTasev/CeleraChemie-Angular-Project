d6b0eba9da00531f4feb0a7096ee2f4e01ee8941
AlexTasev <at@nabo.as>
2020-03-15 13:36:46 +0200

    feat: spinner added

--- celera-chemie-web-project/src/app/app.component.ts
+++ celera-chemie-web-project/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cc-app',
+  selector: 'app-celera-chemie',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
